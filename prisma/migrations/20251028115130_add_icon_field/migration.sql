6b1b83d2de43ac55a9d481e6ef7eaafa6a558302
aleksandrajankovic <aleksandra.bgd.87@gmail.com>
2025-11-20 13:16:30 +0100

    Switch to Neon Postgres

--- prisma/migrations/20251028115130_add_icon_field/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "SpecialPromotion" ADD COLUMN "icon" TEXT DEFAULT '';
-
--- AlterTable
-ALTER TABLE "WeeklyPromotion" ADD COLUMN "icon" TEXT DEFAULT '';
--- prisma/migrations/20251118121545_add_translations_json/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "SpecialPromotion" ADD COLUMN "translations" JSONB;
-
--- AlterTable
-ALTER TABLE "WeeklyPlan" ADD COLUMN "translations" JSONB;
-
--- AlterTable
-ALTER TABLE "WeeklyPromotion" ADD COLUMN "translations" JSONB;
